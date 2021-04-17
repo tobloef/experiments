aa568e30775018101fc21ca9d00eba683d7d9f30
Tobias Løfgren <tobloef@gmail.com>
2021-04-17 19:53:52 +0200

    Add index page

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const path = require("path");
 const express = require("express");
 const ejs = require("ejs");
+const minimist = require("minimist");
 const poetry = require("./poetry");
 
-const PORT = 3000;
+const argv = minimist(process.argv.slice(2));
+
+const PORT = argv.port || 3000;
+const BASE_PATH = argv.basePath || "/";
 
 const app = express();
 
 app.set("views", __dirname);
 app.set("view engine", "ejs");
+app.use(BASE_PATH, (() => {
+	const router = express.Router();
 
-app.get("/poetry", (req, res) => {
-	const poem = poetry.generate();
-	res.render("poetry/index", { poem });
-});
+	router.get("/", (req, res) => {
+		res.render("index", { basePath: BASE_PATH + "/" });
+	});
+
+	router.get("/poetry", (req, res) => {
+		const poem = poetry.generate();
+		res.render("poetry/index", { poem });
+	});
+
+	return router;
+})());
 
 app.listen(PORT, async () => {
 	console.info("Initializing...");
 	await poetry.initialize();
-	console.info(`Started server on port ${PORT}.`);
+	console.info(`Started server on port ${PORT} with base path ${BASE_PATH}.`);
 });
